03c9db939cc416283d0e473b6e4eae4cb85be8a7
raghu1016 <raghurukel1016@gmail.com>
2024-04-25 13:27:14 +0530

    New change

--- api/configuration/dbConfig.js
+++ api/configuration/dbConfig.js
@@ -2,8 +2,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const config = {
-    user: 'postgres',
-    host: 'localhost',
+    user: process.env.USER,
+    host: process.env.HOST,
     database: process.env.DB_Name,
     password: process.env.DB_Password,
     port: process.env.DB_PORT,
